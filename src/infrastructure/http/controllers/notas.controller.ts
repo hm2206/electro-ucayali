f4422853bdbe3e45ae64db073df0140a1432edcf
Hans <twd2206@gmail.com>
2022-11-28 15:51:49 -0500

    feat: add metadata

--- src/infrastructure/http/controllers/notas.controller.ts
+++ src/infrastructure/http/controllers/notas.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { of } from 'rxjs';
 import { NotaCreateService } from 'src/application/notas/nota-create.service';
 import { TypeormUnitOfWork } from 'src/infrastructure/database/unit-of-works/typeorm.unit-of-work';
 import { NotaCreateDto } from '../dtos/nota-create.dto';
 
-@Controller()
+@ApiTags('Notas')
+@Controller('notas')
 export class NotasController {
   constructor(private unitOfWork: TypeormUnitOfWork) {}
 
