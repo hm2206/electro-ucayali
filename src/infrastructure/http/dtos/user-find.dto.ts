43a6eb28a626231cb7a37ffd593aeb5a9f3322d4
Hans <twd2206@gmail.com>
2023-01-08 16:48:44 -0500

    fix uuid valid

--- src/infrastructure/http/dtos/user-find.dto.ts
+++ src/infrastructure/http/dtos/user-find.dto.ts
@@ -1,13 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsDefined, IsUUID } from 'class-validator';
+import { IsDefined } from 'class-validator';
 import { UserFindRequest } from 'src/application/users/user-find.service';
 import { IdentifyUUID } from 'src/domain/value-objects/identify-uuid';
 
 export class UserFindDto implements UserFindRequest {
   @ApiProperty()
   @IsDefined()
-  @IsUUID(4)
   @Transform(({ value }) => new IdentifyUUID(value))
   id: IdentifyUUID;
 }
