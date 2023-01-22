2b86f0d67b7c6adb836148040fa26630d4d8ba1d
Hans <twd2206@gmail.com>
2023-01-22 18:36:09 -0500

    fix edit medidas

--- src/application/medidas/medida-edit.service.ts
+++ src/application/medidas/medida-edit.service.ts
@@ -1,5 +1,6 @@
 import { Medida } from 'src/domain/entities/medida';
 import { IdentifyUUID } from 'src/domain/value-objects/identify-uuid';
+import { NoneEmptyString } from 'src/domain/value-objects/none-empty-string';
 import { IBaseServiceInterface } from 'src/shared/interfaces/base-service.interface';
 import { IUnitOfWorkInterface } from 'src/shared/interfaces/unit-of-work';
 
@@ -9,14 +10,11 @@ export class MedidaEditService implements IBaseServiceInterface {
   async execute(request: MedidaEditRequest) {
     const medidaRepository = this.unitOfWork.medidaRepository;
     const medida = new Medida();
-    medida.load(request);
     medida.setId(request.id);
+    medida.setName(new NoneEmptyString(request.name));
+    medida.setDescription(new NoneEmptyString(request.description));
     await this.unitOfWork.start();
-    return medidaRepository.save({
-      id: medida.getId(),
-      name: medida.getName(),
-      description: medida.getDescription(),
-    });
+    return medidaRepository.save(medida);
   }
 }
 
--- src/infrastructure/http/dtos/medida-edit.dto.ts
+++ src/infrastructure/http/dtos/medida-edit.dto.ts
@@ -1,8 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
 import { IsDefined } from 'class-validator';
 import { MedidaEditRequest } from 'src/application/medidas/medida-edit.service';
-import { NoneEmptyString } from 'src/domain/value-objects/none-empty-string';
 
 export class MedidaEditDto extends MedidaEditRequest {
   @ApiProperty()
@@ -11,6 +9,5 @@ export class MedidaEditDto extends MedidaEditRequest {
 
   @ApiProperty()
   @IsDefined()
-  @Transform(({ value }) => new NoneEmptyString(value))
   description: string;
 }
